98411bf9d6be8d72de41438bff179470cd90b832
Jelson J <jelson20outlook.com>
2020-11-28 17:41:52 +0000

    update

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 'use strict'
 
-function* color(){
-  'Bule'; 
-  'Yellow';
-  'Black';
-  'Green';
-  'Red';
-  'White'
+function* fruts(){
+  yield 'Avacado'; 
+  yield 'Appale';
+  yield 'Apricots';
+  yield 'Banana';
+  yield 'Blackberries';
+  yield 'Blueberries';
+  yield 'Breadfruit';
+  yield 'Cantaloupe';
+  yield 'Carambola';
+  yield 'Cherimoya';
+  yield 'Date Fruit';
+  yield 'Durian';
+  yield 'Elderberries';
+  yield 'Feijoa';
+  yield 'Grapes';
+  yield 'Guava';
+  yield 'Honeydew Melon';
+  yield 'Jackfruit';
+  yield 'Java-Plum'
 }
-for()
+for(let myColors of color()){
+  console.log(myColors)
+}
